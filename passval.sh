2b280db34f0bfd055dba977ea15cb02b93937d50
Rahul <rahulbapumore@gmail.com>
2020-05-05 23:23:44 +0530

    First name validation

---
+++ passval.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+read -p "Enter first name" fname
+fnamepat="^[A-Z]([a-z][a-z])+$";
+if [[ $fname =~ $fnamepat   ]]
+then
+  echo "Valid first name";
+else
+  echo "Invalid first name";
+fi
