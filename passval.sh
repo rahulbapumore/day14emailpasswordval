96311347867ab44e102171a85c2f8bb76ca21724
Rahul <rahulbapumore@gmail.com>
2020-05-05 23:28:32 +0530

    last name validation

--- passval.sh
+++ passval.sh
@@ -8,3 +8,14 @@ then
 else
   echo "Invalid first name";
 fi
+
+
+read -p "Enter Last name" lname
+lnamepat="^[A-Z]([a-z][a-z])+$"";
+if [[ $lname =~ $lnamepat   ]]
+then
+  echo "Valid last name";
+else
+  echo "Invalid last name";
+fi
+
